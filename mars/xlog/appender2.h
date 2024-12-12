f7c95cfb91770f7f2618879950820235aa9af174
fengxing <fengxing@bwoil.com>
2024-12-12 14:11:44 +0800

    fmt format

--- mars/xlog/appender2.h
+++ mars/xlog/appender2.h
@@ -4,7 +4,6 @@ extern MARS_EXPORT char* xlog2_tag1;
 extern MARS_EXPORT char* xlog2_tag2;
 
 namespace mars {
-
 namespace xlog2 {
 
 MARS_EXPORT void appender_open1(const mars::xlog::XLogConfig& _config);
@@ -17,15 +16,6 @@ MARS_EXPORT void appender_set_console_log2(bool _is_open);
 
 MARS_EXPORT void appender_flush();
 MARS_EXPORT void appender_flush_sync();
-
-}
-
-namespace xlog {
-
-#define __xlogger_cpp_impl_tag2(level, tag, ...) xlogger2(level, tag, __XFILE__, __XFUNCTION__, __LINE__, __VA_ARGS__)
-
-#define xinfo2_tag1(...) __xlogger_cpp_impl_tag2(kLevelInfo, xlog2_tag1, __VA_ARGS__)
-#define xinfo2_tag2(...) __xlogger_cpp_impl_tag2(kLevelInfo, xlog2_tag2, __VA_ARGS__)
 
 }
 }
--- samples/log/main.cpp
+++ samples/log/main.cpp
@@ -5,7 +5,28 @@
 
 #include "mars/xlog/appender2.h"
 
+
+#include <fmt/format.h>
+#include <iostream>
+
 using namespace mars::xlog;
+
+namespace xlog {
+
+#define xloggerfmt2(level, tag, file, func, line, ...)                                                                   \
+    if ((!xlogger_IsEnabledFor(level)))                                                                               \
+        ;                                                                                                             \
+    else {                                                                                                            \
+        XLogger(level, tag, file, func, line, false, XLOGGER_HOOK)                                                    \
+        .WriteNoFormat(fmt::format(__VA_ARGS__).c_str());                                                      \
+    }    
+
+#define __xlogger_cpp_implfmt_tag2(level, tag, ...) xloggerfmt2(level, tag, __XFILE__, __XFUNCTION__, __LINE__, __VA_ARGS__)
+
+#define xinfofmt2_tag1(...) __xlogger_cpp_implfmt_tag2(kLevelInfo, xlog2_tag1, __VA_ARGS__)
+#define xinfofmt2_tag2(...) __xlogger_cpp_implfmt_tag2(kLevelInfo, xlog2_tag2, __VA_ARGS__)
+
+}
 
 int main()
 {
@@ -34,10 +55,11 @@ int main()
     mars::xlog2::appender_open1(config1);
     mars::xlog2::appender_open2(config2);
 
-    for (int i = 0; i < 100; i++) {
-
-        xinfo2_tag1("old info aaaa");
-        xinfo2_tag2("old bbbbb info=%0", 0);
+    for (int i = 0; i < 10; i++) {
+        fmt::print("old bbbbb info={}", "elo");
+        //xinfo2_tag1("old info aaaa");
+        xinfofmt2_tag1("old bbbbb info={}", "elo");
+        xinfo2("hello %s", "hkl");
 
     }
     xinfo2("info=1");
